457364e546572c02b71d8ccea1e91db17a7e14b2
Iuri de Silvio <iurisilvio@gmail.com>
2022-10-20 13:38:32 -0700

    Command clone_test_db works.

---
+++ django_migrations_ci/management/commands/clone_test_db.py
@@ -0,0 +1,33 @@
+from django.conf import settings
+from django.core.management.base import BaseCommand
+from django.db import connections
+from django.test.runner import get_max_test_processes
+
+
+# TODO: No idea how to test it.
+class Command(BaseCommand):
+    """
+    Copy the test database to support parallel tests.
+    """
+
+    def add_arguments(self, parser):
+        parser.add_argument("parallel")
+        # pytest-django uses test_db_gwN, from 1 to N worker processes.
+        parser.add_argument("-s", "--suffix", default="")
+
+    def handle(self, parallel, *args, suffix, **options):
+        if parallel == "auto":
+            parallel = get_max_test_processes()
+        else:
+            parallel = int(parallel)
+
+        connection = connections["default"]
+        settings.DATABASES[connection.alias]["NAME"] = connection.creation._get_test_db_name()
+
+        # https://github.com/pytest-dev/pytest-django/blob/e0c77b391ea54c3b8d6ffbb593aa25188a0ce7e9/pytest_django/fixtures.py#L61
+        for index in range(1, parallel + 1):
+            connection.creation.clone_test_db(
+                suffix=f"{suffix}{index}",
+                verbosity=True,
+                keepdb=False,
+            )
---
+++ django_migrations_ci/tests/test_clone_database.py
@@ -0,0 +1,12 @@
+import os
+
+from django.apps import apps
+from django.core.management import execute_from_command_line
+
+
+def test_clone_database():
+    # TODO: Cleanup files before the test.
+    execute_from_command_line(["manage.py", "clone_test_db", "2", "--suffix", "foo"])
+    assert os.path.exists("dbtest_foo1.sqlite3")
+    assert os.path.exists("dbtest_foo2.sqlite3")
+    assert not os.path.exists("dbtest_foo3.sqlite3")
