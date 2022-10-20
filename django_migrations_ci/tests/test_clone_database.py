388b6a03e596319deffb7c4b84ca770eabf78a4b
Iuri de Silvio <iurisilvio@gmail.com>
2022-10-20 15:39:17 -0700

    Fix test clone setup.

--- django_migrations_ci/tests/test_clone_database.py
+++ django_migrations_ci/tests/test_clone_database.py
@@ -5,6 +5,7 @@ from django.core.management import execute_from_command_line
 
 def test_clone_database():
     # TODO: Cleanup files before the test.
+    execute_from_command_line(["manage.py", "setup_test_db"])
     execute_from_command_line(["manage.py", "clone_test_db", "2", "--suffix", "foo"])
     assert os.path.exists("dbtest_foo1.sqlite3")
     assert os.path.exists("dbtest_foo2.sqlite3")
