66175c1bf46333af8f73cc27a9bd652654f9dd42
Iuri de Silvio <iurisilvio@gmail.com>
2022-10-24 10:05:19 -0300

    Cleanup files before tests.

--- django_migrations_ci/tests/conftest.py
+++ django_migrations_ci/tests/conftest.py
@@ -6,3 +6,15 @@ import pytest
 @pytest.fixture(scope="session", autouse=True)
 def setup_env():
     os.environ["DJANGO_SETTINGS_MODULE"] = "django_migrations_ci.tests.testapp.settings"
+
+
+@pytest.fixture(autouse=True)
+def remove_cached_files():
+    os.remove("migrateci-default")
+
+
+@pytest.fixture(autouse=True)
+def remove_sqlite3_files():
+    filename = "dbtest.sqlite3"
+    if os.path.exists(filename):
+        os.remove(filename)
