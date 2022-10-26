920748ddd329a54ac0e3cd7b81998ded33471d1f
Iuri de Silvio <iurisilvio@gmail.com>
2022-10-26 14:05:06 -0300

    :facepalm:

--- django_migrations_ci/backends/sqlite3.py
+++ django_migrations_ci/backends/sqlite3.py
@@ -8,10 +8,11 @@ def load(alias, input_file):
     db_name = db_conf["TEST"]["NAME"]
 
     print(f"Connecting to sqlite3 {db_name}.")
-    with sqlite3.connect(db_name) as conn:
-        with open(input_file, "r") as f:
-            sql = f.read()
-            print(f"Load SQL to {db_name}: {sql}")
+    with open(input_file, "r") as f:
+        sql = f.read()
+        print(f"Load SQL to {db_name}: {sql}")
+
+        with sqlite3.connect(db_name) as conn:
             conn.executescript(sql)
 
 
@@ -21,7 +22,8 @@ def dump(alias, output_file):
 
     print(f"Connecting to sqlite3 {db_name}.")
     with sqlite3.connect(db_name) as conn:
+        sql = (f"{sql}\n" for sql in conn.iterdump())
+        print(f"Dump SQL to {db_name}: {sql}")
+
         with open(output_file, "w") as f:
-            sql = f.writelines()
-            print(f"Dump SQL to {db_name}: {sql}")
             f.writelines(sql)
