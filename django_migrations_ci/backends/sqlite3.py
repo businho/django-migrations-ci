2934032ae17c20bed41cdaebe07248a9cae06ee9
Iuri de Silvio <iurisilvio@gmail.com>
2022-10-26 13:51:33 -0300

    Make it verbose.

--- django_migrations_ci/backends/sqlite3.py
+++ django_migrations_ci/backends/sqlite3.py
@@ -7,15 +7,21 @@ def load(alias, input_file):
     db_conf = settings.DATABASES[alias]
     db_name = db_conf["TEST"]["NAME"]
 
+    print(f"Connecting to sqlite3 {db_name}.")
     with sqlite3.connect(db_name) as conn:
         with open(input_file, "r") as f:
-            conn.executescript(f.read())
+            sql = f.read()
+            print(f"Load SQL to {db_name}: {f.read()}")
+            conn.executescript(sql)
 
 
 def dump(alias, output_file):
     db_conf = settings.DATABASES[alias]
     db_name = db_conf["TEST"]["NAME"]
 
+    print(f"Connecting to sqlite3 {db_name}.")
     with sqlite3.connect(db_name) as conn:
         with open(output_file, "w") as f:
-            f.writelines(f"{sql}\n" for sql in conn.iterdump())
+            sql = f.writelines()
+            print(f"Dump SQL to {db_name}: {f.read()}")
+            f.writelines(sql)
