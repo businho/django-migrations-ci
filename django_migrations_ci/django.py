7b74b1e8fb8e67dddbd511669dbb1a66d384be26
Iuri de Silvio <iurisilvio@gmail.com>
2022-10-29 08:14:06 -0300

    Override database name per test.

--- django_migrations_ci/django.py
+++ django_migrations_ci/django.py
@@ -1,9 +1,7 @@
-from contextlib import contextmanager
 import importlib
 import os
 import re
 
-from django.conf import settings
 from django.db import connections
 from django.test.utils import setup_databases
 
@@ -32,49 +30,27 @@ def setup_test_db():
 def clone_test_db(parallel, is_pytest=False, database="default"):
     connection = connections[database]
 
-    with _override_database_name(connection):
-        for index in range(parallel):
-            if is_pytest:
-                # pytest-django use test_db_gwN, from 0 to N-1.
-                # e.g. test_db_gw0, test_db_gw1, ...
-                # https://github.com/pytest-dev/pytest-django/blob/e0c77b391ea54c3b8d6ffbb593aa25188a0ce7e9/pytest_django/fixtures.py#L61  # noqa: E501
-                suffix = f"gw{index}"
-            else:
-                # Django use test_db_N, from 1 to N.
-                # e.g. test_db_1, test_db_2, ...
-                suffix = f"{index + 1}"
+    for index in range(parallel):
+        if is_pytest:
+            # pytest-django use test_db_gwN, from 0 to N-1.
+            # e.g. test_db_gw0, test_db_gw1, ...
+            # https://github.com/pytest-dev/pytest-django/blob/e0c77b391ea54c3b8d6ffbb593aa25188a0ce7e9/pytest_django/fixtures.py#L61  # noqa: E501
+            suffix = f"gw{index}"
+        else:
+            # Django use test_db_N, from 1 to N.
+            # e.g. test_db_1, test_db_2, ...
+            suffix = f"{index + 1}"
 
-            connection.creation.clone_test_db(
-                suffix=suffix, verbosity=True, keepdb=False
-            )
+        connection.creation.clone_test_db(suffix=suffix, verbosity=True, keepdb=False)
 
-            if is_pytest and connection.vendor == "sqlite":
-                settings_dict = connection.creation.get_test_db_clone_settings(suffix)
-                django_db_name = settings_dict["NAME"]
+        if is_pytest and connection.vendor == "sqlite":
+            settings_dict = connection.creation.get_test_db_clone_settings(suffix)
+            django_db_name = settings_dict["NAME"]
 
-                if "." in django_db_name:
-                    # Django clone_test_db create file db_gw0.sqlite3, but pytest-django
-                    # expects db.sqlite3_gw0. Lets rename the file.
-                    pytest_db_name = re.sub(
-                        r"(_gw\d+)\.(.+)$", r".\2\1", django_db_name
-                    )
+            if "." in django_db_name:
+                # Django clone_test_db create file db_gw0.sqlite3, but pytest-django
+                # expects db.sqlite3_gw0. Lets rename the file.
+                pytest_db_name = re.sub(r"(_gw\d+)\.(.+)$", r".\2\1", django_db_name)
 
-                    # Move db_gw0.sqlite3 to db.sqlite3_gw0.
-                    os.rename(django_db_name, pytest_db_name)
-
-
-@contextmanager
-def _override_database_name(connection):
-    database = connection.alias
-    database_name = settings.DATABASES[database]["NAME"]
-    test_database_name = connection.creation._get_test_db_name()
-
-    connection.close()
-    settings.DATABASES[database]["NAME"] = test_database_name
-    connection.settings_dict["NAME"] = test_database_name
-
-    yield
-
-    connection.close()
-    settings.DATABASES[database]["NAME"] = database_name
-    connection.settings_dict["NAME"] = database_name
+                # Move db_gw0.sqlite3 to db.sqlite3_gw0.
+                os.rename(django_db_name, pytest_db_name)
