ffbc631047ac75a3d6af07add435be540f7ef81c
Iuri de Silvio <iurisilvio@gmail.com>
2022-10-31 13:28:43 -0300

    Use Django connection for sqlite3 dump.

--- django_migrations_ci/backends/sqlite.py
+++ django_migrations_ci/backends/sqlite.py
@@ -1,13 +1,8 @@
-import sqlite3
-
-
 def dump(connection, output_file):
-    db_name = connection.settings_dict["TEST"]["NAME"]
+    connection.ensure_connection()
+    sql = "".join(f"{sql}\n" for sql in connection.connection.iterdump())
 
-    print(f"Connecting to sqlite3 {db_name}.")
-    with sqlite3.connect(db_name) as conn:
-        sql = "".join(f"{sql}\n" for sql in conn.iterdump())
-        print(f"Dump SQL to {db_name}")
-
-        with open(output_file, "w") as f:
-            f.write(sql)
+    db_name = connection.settings_dict["NAME"]
+    print(f"Dump {db_name} SQL to {output_file}")
+    with open(output_file, "w") as f:
+        f.write(sql)
