650a5ebcafd376ed7ee59a2fbdf23e6461e412d7
Iuri de Silvio <iurisilvio@gmail.com>
2023-01-08 15:13:01 -0300

    Lots of logs.

--- django_migrations_ci/management/commands/migrateci.py
+++ django_migrations_ci/management/commands/migrateci.py
@@ -1,4 +1,5 @@
 import itertools
+import logging
 
 from django.core.files.storage import get_storage_class
 from django.core.management.base import BaseCommand
@@ -15,6 +16,8 @@ except ImportError:
 
 
 from django_migrations_ci import django
+
+logger = logging.getLogger(__name__)
 
 
 class Command(BaseCommand):
@@ -50,31 +53,56 @@ class Command(BaseCommand):
         elif parallel is not None:
             parallel = int(parallel)
 
-        storage = storage_class(directory)
+        if verbosity >= 2:
+            logger.info(f"Using storage {storage_class=} on {directory=}.")
+        storage = storage_class(location=directory)
+
         _, files = storage.listdir("")
         files = set(files)
+        if verbosity >= 3:
+            logger.info(f"Files in storage: {files}")
 
         unique_connections = django.get_unique_connections()
 
         current_checksum = None
-        for cached_checksum in itertools.islice(django.hash_files(), depth):
+        checksums = itertools.islice(django.hash_files(), depth)
+        for cached_checksum in checksums:
             # Current checksum is the first result returned from hash_files.
             if current_checksum is None:
                 current_checksum = cached_checksum
+                if verbosity:
+                    logger.info(f"Migrations current checksum is {current_checksum}.")
 
             cached_files = {
                 connection.alias: f"migrateci-{connection.alias}-{cached_checksum}"
                 for connection in unique_connections
             }
             if all(f in files for f in cached_files.values()):
-                print("Database cache exists.")
+                if verbosity:
+                    logger.info(
+                        f"Migrations cache found with checksum {cached_checksum}."
+                    )
+                if verbosity >= 3:
+                    for other_checksum in checksums:
+                        logger.info(
+                            f"Calculated checksum {other_checksum} not evaluated."
+                        )
                 break
+
+            if verbosity >= 2:
+                logger.info(
+                    f"Migrations cache NOT found for checksum {cached_checksum}."
+                )
+
         else:
             cached_checksum = None
             cached_files = None
-            print("Database cache does not exist.")
+            if verbosity:
+                logger.info("Database cache does not exist.")
 
         if cached_files:
+            if verbosity >= 2:
+                logger.info("Create test db from cache.")
             django.create_test_db(verbosity=verbosity)
             for connection in unique_connections:
                 cached_file = cached_files[connection.alias]
@@ -82,6 +110,11 @@ class Command(BaseCommand):
                     django.load(connection, cached_file, storage)
 
         if current_checksum != cached_checksum:
+            if verbosity >= 2:
+                logger.info(
+                    f"Setup test db from {cached_checksum=} to {current_checksum=}."
+                )
+
             django.setup_test_db(verbosity=verbosity)
             for connection in unique_connections:
                 current_file = f"migrateci-{connection.alias}-{current_checksum}"
@@ -89,6 +122,8 @@ class Command(BaseCommand):
                     django.dump(connection, current_file, storage)
 
         if parallel:
+            if verbosity >= 2:
+                logger.info(f"Clone test db for {parallel=}.")
             for connection in unique_connections:
                 with django.test_db(connection):
                     django.clone_test_db(
