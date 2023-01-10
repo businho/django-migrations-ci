350a232863ed11fd0912dc7b35f04c62686b3fa0
Iuri de Silvio <iurisilvio@gmail.com>
2023-01-10 09:46:54 -0300

    Settings to avoid command lines.

---
+++ django_migrations_ci/settings.py
@@ -0,0 +1,10 @@
+from django.conf import settings
+
+storage_class = getattr(
+    settings,
+    "MIGRATECI_STORAGE",
+    "django.core.files.storage.FileSystemStorage",
+)
+location = getattr(settings, "MIGRATECI_LOCATION", "")
+depth = getattr(settings, "MIGRATECI_DEPTH", 0)
+is_pytest = getattr(settings, "MIGRATECI_PYTEST", False)
