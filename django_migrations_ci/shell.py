bd6142340d5fb892a52f2d5f3edde9f9a35500b4
Iuri de Silvio <iurisilvio@gmail.com>
2024-07-24 16:40:34 +0200

    Propagate env variables to shell subprocess to respect `PATH` (#58)

--- django_migrations_ci/shell.py
+++ django_migrations_ci/shell.py
@@ -1,4 +1,5 @@
 import logging
+import os
 from subprocess import PIPE, Popen
 
 logger = logging.getLogger(__name__)
@@ -10,13 +11,18 @@ class MigrateCIShellException(Exception):
 
 def exec(command, env=None):
     logger.info(f"Running shell command {command}")
+
+    subprocess_env = os.environ.copy()
+    if env:
+        subprocess_env.update(env)
+
     p = Popen(
         command,
         shell=True,
         stdin=PIPE,
         stdout=PIPE,
         stderr=PIPE,
-        env=env,
+        env=subprocess_env,
     )
     stdout, stderr = p.communicate()
     if stderr:
